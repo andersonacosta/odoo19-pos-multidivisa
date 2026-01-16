42ef94900dd5e73b9471765d039ab34d5ee01fee
andersonacosta <andersonacosta@hotmail.com>
2026-01-15 22:30:39 -0400

    Update __manifest__.py

--- pos_multidivisa/__manifest__.py
+++ pos_multidivisa/__manifest__.py
@@ -5,16 +5,18 @@
     'category': 'Point of Sale',
     'summary': 'POS multi-divisa dinámico con cambio de moneda principal',
     'description': """
-    Sistema POS Multi-Divisa Dinámico para Odoo 19 CE.
-    Permite cambiar la moneda operativa principal (USD/EUR) con un clic,
-    ajustando listas de precios, visualización y referencias BCV sin afectar la contabilidad en VES.
-    """,
+Sistema POS Multi-Divisa Dinámico para Odoo 19 CE.
+Permite cambiar la moneda operativa principal (USD/EUR) con un clic,
+ajustando listas de precios, visualización y referencias BCV sin afectar la contabilidad en VES.
+""",
     'author': 'Desarrollador',
     'license': 'LGPL-3',
-    'depends': ['point_of_sale','account'],
+    'depends': ['point_of_sale', 'account'],
     'data': [
+        'security/ir.model.access.csv',
         'data/ir_cron.xml',
         'views/res_config_settings_views.xml',
+        'views/fiscal_book_views.xml',
     ],
     'qweb': [
         'static/src/xml/pos_currency_widget.xml',
