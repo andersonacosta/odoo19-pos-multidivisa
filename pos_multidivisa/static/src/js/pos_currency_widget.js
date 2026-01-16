bf30ae7802f0daa89169bb54c5422eb7f6c1142a
andersonacosta <andersonacosta@hotmail.com>
2026-01-15 22:49:18 -0400

    Update pos_currency_widget.js

--- pos_multidivisa/static/src/js/pos_currency_widget.js
+++ pos_multidivisa/static/src/js/pos_currency_widget.js
@@ -1,19 +1,22 @@
-odoo.define('pos_multidivisa.PosCurrencyWidget', function(require){
+odoo.define('pos_multidivisa.PosCurrencyWidget', function (require) {
     'use strict';
-    const { useState } = owl.hooks;
-    const { PosComponent } = require('point_of_sale.PosComponent');
+    const { useState, useRef } = owl.hooks;
+    const PosComponent = require('point_of_sale.PosComponent');
     const rpc = require('web.rpc');
 
     class PosCurrencyWidget extends PosComponent {
-        constructor() {
-            super(...arguments);
+        setup() {
+            super.setup();
             this.state = useState({
                 primaryRate: 0,
                 secondaryRate: 0,
                 mode: 'auto',
+                statusIcon: '🟢',
             });
+            this.inputRef = useRef('input');
             this.loadRates();
         }
+
         async loadRates() {
             try {
                 const result = await rpc.query({
@@ -25,11 +28,14 @@ odoo.define('pos_multidivisa.PosCurrencyWidget', function(require){
                     this.state.primaryRate = result.primary_rate;
                     this.state.secondaryRate = result.secondary_rate;
                     this.state.mode = result.mode;
+                    this.state.statusIcon = result.mode === 'auto' ? '🟢' : '🟠';
                 }
             } catch (error) {
                 console.error(error);
+                this.state.statusIcon = '🔴';
             }
         }
+
         async toggleMode() {
             await rpc.query({
                 model: 'res.currency',
@@ -38,7 +44,20 @@ odoo.define('pos_multidivisa.PosCurrencyWidget', function(require){
             });
             await this.loadRates();
         }
+
+        async setManualRate(value) {
+            const rate = parseFloat(value);
+            if (!isNaN(rate)) {
+                await rpc.query({
+                    model: 'res.currency',
+                    method: 'set_pos_currency_rate',
+                    args: [rate],
+                });
+                await this.loadRates();
+            }
+        }
     }
+
     PosCurrencyWidget.template = 'PosCurrencyWidget';
     return PosCurrencyWidget;
 });
